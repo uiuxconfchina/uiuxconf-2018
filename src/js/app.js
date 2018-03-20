44fbf4de679c4dddbbb454b3a297b4c9b94a8401
natalia <natalia@wiredcraft.com>
2018-03-21 00:23:31 +0800

    enable toggling menu and language

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,35 @@
 // JS Goes here - ES6 supported
 
-// Say hello
-console.log("🦊 Hello! Edit me in src/js/app.js");
+const toggleMenu = () => {
+  const menu = document.getElementById('menu')
+  const button = document.getElementById('menuToggle')
+  menu.classList.contains('u-hidden')
+  ? (menu.classList.remove('u-hidden'), button.classList.add('c-navbar__toggle_close'), button.classList.remove('c-navbar__toggle_hamburger'))
+  : (menu.classList.add('u-hidden'), button.classList.add('c-navbar__toggle_hamburger'), button.classList.remove('c-navbar__toggle_close'))
+}
+
+const addToggleMenuListener = () => {
+  const button = document.getElementById('menuToggle')
+  button.addEventListener('click', toggleMenu)
+}
+
+const toggleLanguage = (lang) => {
+  const button = document.getElementById('langToggle')
+  const path = window.location.pathname
+  const newPath = `/${lang}${path}`
+  window.location.pathname = newPath
+  lang === 'cn' ? button.setAttribute('data-lang', 'en') : button.setAttribute('data-lang', 'cn')
+}
+
+const addToggleLanguageListener = () => {
+  const button = document.getElementById('langToggle')
+  const lang = button.getAttribute('data-lang')
+  button.addEventListener('click', () => toggleLanguage(lang))
+}
+
+const init = () => {
+  addToggleMenuListener()
+  addToggleLanguageListener()
+}
+
+init()
