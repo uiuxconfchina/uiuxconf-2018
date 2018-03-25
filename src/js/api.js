6b3deb8c80a89b1d066bf617a6df1a25fd63f16d
Shuaibird Hwang <shuaibird@icloud.com>
2018-03-25 12:54:25 +0800

    correct the endpoint of the createSponsor api

--- src/js/api.js
+++ src/js/api.js
@@ -4,6 +4,5 @@ const host = 'https://hooks.zapier.com/hooks/catch/66762'
 
 const genPostApi = (endpoint) => (payload) => $.post(`${host}${endpoint}`, payload)
 
-// export const createSponsor = genPostApi('/kfv3ii')
-export const createSponsor = genPostApi('/kf')
+export const createSponsor = genPostApi('/kfv3ii')
 export const createSpeaker = genPostApi('/kaylki')
