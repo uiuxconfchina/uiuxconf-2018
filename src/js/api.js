4a03c7edfb1d87527b8a98eb2883d532df8215d9
Shuaibird Hwang <shuaibird@icloud.com>
2018-03-25 12:47:45 +0800

    add two APIs for the forms: createSponsor and createSpeaker

---
+++ src/js/api.js
@@ -0,0 +1,9 @@
+import $ from 'jquery'
+
+const host = 'https://hooks.zapier.com/hooks/catch/66762'
+
+const genPostApi = (endpoint) => (payload) => $.post(`${host}${endpoint}`, payload)
+
+// export const createSponsor = genPostApi('/kfv3ii')
+export const createSponsor = genPostApi('/kf')
+export const createSpeaker = genPostApi('/kaylki')
