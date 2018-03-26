18fd243ac1ce97bd75f544f96d8d0264a1a1fd74
Shuaibird Hwang <shuaibird@icloud.com>
2018-03-26 12:49:59 +0800

    add uploadImg api

--- src/js/api.js
+++ src/js/api.js
@@ -6,3 +6,12 @@ const genPostApi = (endpoint) => (payload) => $.post(`${host}${endpoint}`, paylo
 
 export const createSponsor = genPostApi('/kfv3ii')
 export const createSpeaker = genPostApi('/kaylki')
+
+export const uploadImg = (data) =>
+  $.ajax({
+    type: 'POST',
+    url:  '//wiredcraft-com.s3.ap-northeast-2.amazonaws.com/',
+    data,
+    processData: false,
+    contentType: false,
+  })
