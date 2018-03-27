daafd3b188a5687b63c13e8283f514c5154b090c
Shuaibird Hwang <shuaibird@icloud.com>
2018-03-27 12:48:10 +0800

    add abstracted From class

---
+++ src/js/form.js
@@ -0,0 +1,53 @@
+import $ from 'jquery'
+
+export default class Form {
+  // utils for detect whether the element is normal or radio input type
+  // the logic doesn't cover all the edge cases
+  static isTypeRadio = ($el) => $el.length > 1
+
+  _defaultPayload = {}
+
+  constructor(elems) {
+    // validation
+    if (!elems instanceof Object) {
+      throw 'An object should be passed to init the form object'
+    }
+    if (!('form' in elems)) {
+      throw 'The form element should be passed'
+    }
+    for (const field in elems) {
+      const $el = elems[field]
+      if ($el instanceof $) {
+        this[`_${field}`] = elems[field]
+      }
+    }
+    this._defaultPayload = this.getPayload()
+  }
+
+  getPayload = () => {
+    const payload = {}
+    for (const key in this) {
+      if (!/_form/.test(key)) {
+        const $el = this[key]
+        if ($el instanceof $) {
+          payload[key.slice(1)] = Form.isTypeRadio($el) ? $el.filter(':checked').val() : $el.val()
+        }
+      }
+    }
+    return payload
+  }
+
+  setBackToDefault = () => {
+    for (const field in this._defaultPayload) {
+      const $el = this[`_${field}`]
+      const defaultValue = this._defaultPayload[field]
+      if (Form.isTypeRadio($el)) {
+        $el.filter(`[value="${defaultValue}"]`).prop('checked', true)
+      } else {
+        $el.val(defaultValue)
+      }
+    }
+  }
+
+  submit = (...rest) => this._form.submit(...rest)
+}
