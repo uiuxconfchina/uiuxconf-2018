b94fc13f5edd8774bba9fbc4cb4a69130f0b40a7
Shuaibird Hwang <shuaibird@icloud.com>
2018-03-27 12:49:07 +0800

    init the speakerForm and sponsorForm with the Form class

--- src/js/speakerForm.js
+++ src/js/speakerForm.js
@@ -1,80 +1,9 @@
+import Form from './form'
 import {getElemInScope, formatSpeakerPayload} from './utils'
 import {postToZapier, uploadImg} from './api'
 import uuid from 'uuid/v4'
 
 (() => {
-  class SpeakerForm {
-    constructor(elems) {
-      this._form = elems.form
-      this._name = elems.name
-      this._city = elems.city
-      this._email = elems.email
-      this._title = elems.title
-      this._mobile = elems.mobile
-      this._social = elems.social
-      this._bio = elems.bio
-      this._reason = elems.reason
-      this._topic = elems.topic
-      this._desc = elems.desc
-      this._talks = elems.talks
-      this._sponsored = elems.sponsored
-      this._preference = elems.preference
-      this._defaultPayload = this.getPayload()
-    }
-
-    getPayload = () => (
-      {
-        name: this._name.val(),
-        city: this._city.val(),
-        email: this._email.val(),
-        title: this._title.val(),
-        mobile: this._mobile.val(),
-        social: this._social.val(),
-        bio: this._bio.val(),
-        reason: this._reason.val(),
-        topic: this._topic.val(),
-        desc: this._desc.val(),
-        talks: this._talks.val(),
-        sponsored: this._sponsored.filter(':checked').val(),
-        preference: this._preference.filter(':checked').val(),
-      }
-    )
-
-    setBackToDefault = () => {
-      const {
-        name,
-        city,
-        email,
-        title,
-        mobile,
-        social,
-        bio,
-        reason,
-        topic,
-        desc,
-        talks,
-        sponsored,
-        preference,
-      } = this._defaultPayload
-      this._name.val(name)
-      this._city.val(city)
-      this._email.val(email)
-      this._title.val(title)
-      this._mobile.val(mobile)
-      this._social.val(social)
-      this._bio.val(bio)
-      this._reason.val(reason)
-      this._topic.val(topic)
-      this._desc.val(desc)
-      this._talks.val(talks)
-      this._sponsored.filter(`[value="${sponsored}"]`).prop('checked', true)
-      this._preference.filter(`[value="${preference}"]`).prop('checked', true)
-    }
-
-    submit = (...rest) => this._form.submit(...rest)
-  }
-
-
   const getElem = getElemInScope('#speaker-application-form')
   const elems = {
     form: getElem(''),
@@ -93,7 +22,7 @@ import uuid from 'uuid/v4'
     preference: getElem('[name="presentation"]'),
   }
 
-  const speakerForm = new SpeakerForm(elems)
+  const speakerForm = new Form(elems)
   speakerForm.headshot = getElem('[name="headshot"]')
   speakerForm.headshotPreview = getElem('#headshot-preview')
 
--- src/js/sponsorForm.js
+++ src/js/sponsorForm.js
@@ -1,43 +1,8 @@
+import Form from './form'
 import {getElemInScope, formatSponsorPayload} from './utils'
 import {postToZapier} from './api'
 
 (() => {
-  class SponsorForm {
-    constructor(elems) {
-      this._form = elems.form
-      this._name = elems.name
-      this._company = elems.company
-      this._email = elems.email
-      this._phone = elems.phone
-      this._sponsorLevel = elems.sponsorLevel
-      this._comments = elems.comments
-      this._defaultPayload = this.getPayload()
-    }
-
-    getPayload = () => (
-      {
-        name: this._name.val(),
-        company: this._company.val(),
-        email: this._email.val(),
-        phone: this._phone.val(),
-        sponsorLevel: this._sponsorLevel.filter(':checked').val(),
-        comments: this._comments.val(),
-      }
-    )
-
-    setBackToDefault = () => {
-      const {name, company, email, phone, sponsorLevel, comments} = this._defaultPayload
-      this._name.val(name)
-      this._company.val(company)
-      this._email.val(email)
-      this._phone.val(phone)
-      this._sponsorLevel.filter(`[value="${sponsorLevel}"]`).prop('checked', true)
-      this._comments.val(comments)
-    }
-
-    submit = (...rest) => this._form.submit(...rest)
-  }
-
   const getElem = getElemInScope('#sponsor-application-form')
   const elems = {
     form: getElem(''),
@@ -49,7 +14,7 @@ import {postToZapier} from './api'
     comments: getElem('[name="questions"]'),
   }
 
-  const sponsorForm = new SponsorForm(elems)
+  const sponsorForm = new Form(elems)
 
   sponsorForm.submit((evt) => {
     evt.preventDefault()
